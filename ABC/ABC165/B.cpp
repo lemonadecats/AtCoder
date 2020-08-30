50ad428c5360284a8e993314f664ad7546bcf040
lemonadecats <koubee99@gmail.com>
2020-08-30 23:37:36 +0900

    ABC165

--- ABC/ABC165/B.cpp
+++ ABC/ABC165/B.cpp
@@ -3,5 +3,19 @@ using namespace std;
 
 int	main(void)
 {
+	long long x;
+	cin >> x;
+	int ans = 0;
+	long long value = 100;
+	while (1)
+	{
+		if (value >= x)
+		{
+			cout << ans << endl;
+			return (0);
+		}
+		value += (value / 100);
+		ans++;
+	}
 	return (0);
 }
