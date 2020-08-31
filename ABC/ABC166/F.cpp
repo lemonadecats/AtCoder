878f402d6151d508e1f26920072faa86d033f1df
lemonadecats <koubee99@gmail.com>
2020-09-01 00:20:05 +0900

    shorter F.cpp

--- ABC/ABC166/F.cpp
+++ ABC/ABC166/F.cpp
@@ -1,45 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int cnt;
-char ans[100'001];
+int		cnt;
+bool	flag;
+char	ans[100'001];
+string	s[100'001];
 
-void apply2(int *a, int *b, string s, int index)
+void apply(int *a, int *b, string s, int index)
 {
-	(*a)++;
-	(*b)--;
+	(*a)++; (*b)--;
 	ans[cnt++] = s[index];
 }
 
-bool solve2(int *X, int *Y, string s)
+bool solve(int *X, int *Y, string s)
 {
-	if (!(*X) && !(*Y))
-		return (false);
-	if ((*X) > (*Y)) apply2(Y, X, s, 1);
-	else apply2(X, Y, s, 0);
+	if (!(*X) && !(*Y)) return (false);
+	if ((*X) > (*Y)) apply(Y, X, s, 1);
+	else apply(X, Y, s, 0);
 	return (true);
 }
 
 int	main(void)
 {
-	int n, a, b, c;
-	cin >> n >> a >> b >> c;
-	vector<string> s(n + 1);
-	int A, B, C;
-	A = a;
-	B = b;
-	C = c;
+	int n, A, B, C;
+	cin >> n >> A >> B >> C;
 	for (int i = 0; i < n; i++) cin >> s[i];
-	s[n] = "ZZ";
-	bool flag;
+	s[n] = "AA";
 	for (int i = 0; i < n; i++)
 	{
-		if (s[i] == "AB" && s[i + 1] == "BC") flag = solve2(&B, &A, "BA");
-		else if (s[i] == "AB") flag = solve2(&A, &B, "AB");
-		else if (s[i] == "BC" && s[i + 1] == "AC") flag = solve2(&C, &B, "CB");
-		else if (s[i] == "BC") flag = solve2(&B, &C, "BC");
-		else if (s[i] == "AC" && s[i + 1] == "AB") flag = solve2(&A, &C, "AC");
-		else flag = solve2(&C, &A, "CA");
+		if (s[i] == "AB") flag = (s[i + 1] == "BC") ? solve(&B, &A, "BA") : solve(&A, &B, "AB");
+		if (s[i] == "BC") flag = (s[i + 1] == "AC") ? solve(&C, &B, "CB") : solve(&B, &C, "BC");
+		if (s[i] == "AC") flag = (s[i + 1] == "AB") ? solve(&A, &C, "AC") : solve(&C, &A, "CA");
 		if (flag == false)
 		{
 			cout << "No" << endl;
